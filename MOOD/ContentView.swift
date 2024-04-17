f031a5611e29c4f4d3b40801b4eefbebf731300c
Brianca Knight <briancaknight@gmail.com>
2024-04-17 11:47:33 -0700

    update homepage with assets

--- MOOD/ContentView.swift
+++ MOOD/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+       
+                LinearGradient(
+                    gradient: Gradient(stops: [
+                        Gradient.Stop(color: Color(red: 0.99, green: 0.40, blue: 0.61), location: 0.0),
+                        Gradient.Stop(color: Color(red: 0.70, green: 0.47, blue: 1.0), location: 0.33),
+                        Gradient.Stop(color: Color(red: 0.40, green: 0.94, blue: 0.99), location: 0.66),
+                        Gradient.Stop(color: Color(red: 0.60, green: 0.94, blue: 0.36), location: 1.0)
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+                
+                VStack {
+                    Image("clear")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 400, height: 400)
+
+                    Text("BIG feelings?")
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                    Text("Let's learn about your MOOD")
+                        .font(.headline)
+
+                    // NavigationLink(destination: PageView(choiceMade: .constant(0))) {
+                    //     Text("Let's Go!")
+                    // }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
-        .padding()
     }
 }
 
