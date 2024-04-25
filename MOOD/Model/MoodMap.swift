db3bb0ea7011841e39e1ac9f1023a565212b1241
Brianca Knight <briancaknight@gmail.com>
2024-04-25 08:52:30 -0700

    add MoodMap model

---
+++ MOOD/Model/MoodMap.swift
@@ -0,0 +1,47 @@
+//
+//  MoodMap.swift
+//  MOOD
+//
+//  Created by Brianca Knight on 4/25/24.
+//
+
+import Foundation
+
+struct Mood {
+    let title: String
+    let pic: String
+    let paragraph: String
+}
+
+let map = [
+    Mood(title: "Happy",
+         pic: "img string",
+         paragraph: "This is a paragraph about feelings."
+         ),
+    
+    Mood(title: "Sad",
+         pic: "img string",
+         paragraph: "This is a paragraph about feelings."
+         ),
+    
+    Mood(title: "Mad",
+         pic: "img string",
+         paragraph: "This is a paragraph about feelings."
+         ),
+    
+    Mood(title: "Worried",
+         pic: "img string",
+         paragraph: "This is a paragraph about feelings."
+         ),
+    
+    Mood(title: "Surprised",
+         pic: "img string",
+         paragraph: "This is a paragraph about feelings."
+         ),
+    
+    Mood(title: "Dissapointed",
+         pic: "img string",
+         paragraph: "This is a paragraph about feelings."
+         ),
+   
+]
