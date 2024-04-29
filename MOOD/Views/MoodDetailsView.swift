11dbb42fbb2d1599ab26870625023d9389f804c4
Brianca Knight <briancaknight@gmail.com>
2024-04-29 13:24:39 -0700

    add MoodDetailsView

---
+++ MOOD/Views/MoodDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MoodDetailsView.swift
+//  MOOD
+//
+//  Created by Brianca Knight on 4/29/24.
+//
+
+import SwiftUI
+
+struct MoodDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodDetailsView()
+}
